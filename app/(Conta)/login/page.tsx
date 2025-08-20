9bf4e5b4d6d47540ef51c89805845b5fce6fee7d
claudinei05 <oliveiraclaudinei786@gmail.com>
2025-08-20 18:32:30 -0400

    Criar funcionalidade de login

--- app/(Conta)/login/page.tsx
+++ app/(Conta)/login/page.tsx
@@ -1,8 +1,43 @@
 "use client";
 
-import { Container, Typography, TextField, Button, Card, CardContent, Grid } from "@mui/material";
+import {
+  Container,
+  Typography,
+  TextField,
+  Button,
+  Card,
+  CardContent,
+  Grid,
+  Alert,
+} from "@mui/material";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function LoginPage() {
+  const [email, setEmail] = useState<string>("");
+  const [senha, setSenha] = useState<string>("");
+  const [error, setError] = useState<string>("");
+  const router = useRouter();
+
+  const handleLogin = () => {
+    if (!email.trim() || !senha.trim()) {
+      setError("Por favor, preencha todos os campos.");
+      return;
+    }
+
+    const usuariosSalvos = JSON.parse(
+      localStorage.getItem("salvarUsuario") || "[]"
+    );
+
+    const verificarUserExiste = usuariosSalvos.find((e:any)=> e.email === email && e.senha === senha)
+
+    if(verificarUserExiste){
+     router.push("/tarefa")
+    }else {
+      setError("E-mail ou senha incorretos.");
+    }
+  };
+
   return (
     <Container maxWidth="sm" sx={{ mt: 6 }}>
       <Card>
@@ -10,7 +45,11 @@ export default function LoginPage() {
           <Typography variant="h5" component="h1" gutterBottom align="center">
             Login
           </Typography>
-
+          {error && (
+            <Alert severity="error" sx={{ mb: 2 }}>
+              {error}
+            </Alert>
+          )}
           <Grid container spacing={2}>
             {/* E-mail */}
             <Grid item xs={12}>
@@ -19,6 +58,8 @@ export default function LoginPage() {
                 label="E-mail"
                 type="email"
                 variant="outlined"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
               />
             </Grid>
 
@@ -29,6 +70,8 @@ export default function LoginPage() {
                 label="Senha"
                 type="password"
                 variant="outlined"
+                value={senha}
+                onChange={(e) => setSenha(e.target.value)}
               />
             </Grid>
 
@@ -39,6 +82,7 @@ export default function LoginPage() {
                 variant="contained"
                 color="primary"
                 size="large"
+                onClick={handleLogin}
               >
                 Entrar
               </Button>
